11be0aa16062227017f811124e5f137dce93fcf1
Darko Doko <darko.doko1@gmail.com>
2021-11-14 11:24:53 +0100

    Implemented database access for device data

--- src/main/java/com/ddoko/energy_resources/api/DeviceDAO.java
+++ src/main/java/com/ddoko/energy_resources/api/DeviceDAO.java
@@ -1,4 +1,14 @@
 package com.ddoko.energy_resources.api;
 
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.Define;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+
 public interface DeviceDAO {
+
+    @SqlQuery("SELECT state FROM <table> WHERE UUID = :uuid")
+    boolean getDeviceState(@Define("table") String table, @Bind("uuid") String uuid);
+
+    @SqlQuery("INSERT INTO <table> (UUID, STATE) VALUES (:uuid, :charging)")
+    void setDeviceState(@Define("table") String table, @Bind("uuid") String uuid, @Bind("charging") boolean charging);
 }
