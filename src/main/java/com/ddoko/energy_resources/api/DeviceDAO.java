5dba630e9eddc352aa67efcb23bc87c4abe1ed21
Darko Doko <darko.doko1@gmail.com>
2021-12-26 19:12:45 +0100

    Fixed battery state setting

--- src/main/java/com/ddoko/energy_resources/api/DeviceDAO.java
+++ src/main/java/com/ddoko/energy_resources/api/DeviceDAO.java
@@ -3,12 +3,13 @@ package com.ddoko.energy_resources.api;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.Define;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface DeviceDAO {
 
     @SqlQuery("SELECT state FROM <table> WHERE UUID = :uuid")
     boolean getDeviceState(@Define("table") String table, @Bind("uuid") String uuid);
 
-    @SqlQuery("INSERT INTO <table> (UUID, STATE) VALUES (:uuid, :charging)")
+    @SqlUpdate("INSERT INTO <table> (UUID, STATE) VALUES (:uuid, :charging)")
     void setDeviceState(@Define("table") String table, @Bind("uuid") String uuid, @Bind("charging") boolean charging);
 }
